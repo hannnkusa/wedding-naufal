5c91a0149103b628701798cbd03d32d44bb5c110
Kutub <kutubuddin2528@gmail.com>
2024-03-19 10:50:51 +0530

    set up react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import "./App.css";
 // import AppButton from "./shared/components/app_button";
 // import LampContainer from "./shared/landing-page/lamp_container";
 import LandingPage from "./shared/landing-page/landing_page";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <LandingPage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
