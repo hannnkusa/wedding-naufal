6732016adc5d7470303d6ca01af81fdd33931b95
Naufal Algiffary <naufalgiffary@gmail.com>
2024-04-02 23:23:12 +0700

    fix: Adjust wording

--- src/shared/landing-page/landing_page.tsx
+++ src/shared/landing-page/landing_page.tsx
@@ -87,7 +87,7 @@ const LandingPage: FunctionComponent<LandingPageProps> = () => {
     if (isPlaying) {
       audioElement?.pause();
     } else {
-      audioElement?.play();
+      audioElement?.pause();
     }
 
     setIsPlaying(!isPlaying);
@@ -272,57 +272,33 @@ const LandingPage: FunctionComponent<LandingPageProps> = () => {
             <div className="minimalistMagazine_LocationDetailContainer__qK6qn !text-darker-grey !bg-grayish-orange w-full">
               <div className="h-full w-full flex flex-col justify-center items-center py-5">
                 <div className="px-6 py-8 w-full text-center flex flex-col items-center">
-                  <div className="text-2xl font-lagunac text-center mb-10 magazine_LocationTitle__upeqi">
+                  <div className="text-2xl font-camorant font-bold text-center mb-4 magazine_LocationTitle__upeqi">
                     <div>AKAD</div>
                   </div>
                   <div className="text-sm font-lagunac mb-2">
                     MINGGU, 28 APRIL 2024
                   </div>
                   <div className="text-base font-lagunac mb-2">
-                    ・09:00 WIB・
-                  </div>
-                  <div className="text-xs">
-                    <a
-                      className="underline text-light-burgundy font-medium __className_e42d7b"
-                      href="https://www.instagram.com/byintanina/"
-                      rel="noopener"
-                      target="_blank"
-                    >
-                      Lihat Live Streaming
-                    </a>
+                    08.00 - 10.00 WIB
                   </div>
                 </div>
                 <div className="px-6 py-8 w-full text-center flex flex-col items-center">
-                  <div className="text-2xl font-lagunac text-center mb-10 magazine_LocationTitle__upeqi">
+                  <div className="text-2xl font-camorant font-bold text-center mb-4 magazine_LocationTitle__upeqi">
                     <div>RESEPSI</div>
                   </div>
                   <div className="text-sm font-lagunac mb-2">
                     MINGGU, 28 APRIL 2024
                   </div>
                   <div className="text-base font-lagunac mb-2">
-                    ・12:30 - 17:00 WIB・
-                  </div>
-                  <div className="text-xs">
-                    <a
-                      className="underline text-light-burgundy font-medium __className_e42d7b"
-                      href="https://www.instagram.com/byintanina/"
-                      rel="noopener"
-                      target="_blank"
-                    >
-                      Tambahkan ke Kalender
-                    </a>
+                    11.00 - 14.00 WIB
                   </div>
                 </div>
                 <div className="px-6 py-8 w-full text-center flex flex-col items-center">
-                  <div className="text-2xl font-lagunac text-center mb-10">
+                  <div className="text-2xl font-camorant font-bold text-center mb-4">
                     <div>LOKASI</div>
+                    <div className="border-t-2 mt-0  border-blue-950" />
                   </div>
-                  <div className="mb-2 text-sm font-normal">
-                    Mason Pine Hotel
-                  </div>
-                  <div className="text-base font-lagunac mb-2">
-                    ・12:30 - 17:00 WIB・
-                  </div>
+                  <div className="mb-2 font-normal">Mason Pine Hotel</div>
                   <div className="text-xs text-gold mb-6 px-6 leading-normal break-words">
                     Jl. H.R. Soebrantas Km 8,5 Kotak Pos, 1027,
                     <br />
@@ -410,6 +386,29 @@ const LandingPage: FunctionComponent<LandingPageProps> = () => {
               </div>
             </div>
           </section>
+          <div className="group fixed bottom-0 right-0 p-2  flex items-end justify-end w-24 h-24 z-50 ">
+            <div className="text-white shadow-xl flex items-center justify-center p-3 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500 z-50 absolute  ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6 h-6 group-hover:rotate-90 duration-[0.6s]"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+                />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+              </svg>
+            </div>
+          </div>
         </motion.div>
       )}
       <footer
